665adca91c46612728032fcbf7abe1714ad38a1b
LukaszSawala <lukaszsawala2003@gmail.com>
2025-03-17 11:48:01 +0100

    adding grid search module

---
+++ notebooks/models.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)  
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size // 2)
+        self.fc4 = nn.Linear(hidden_size // 2, hidden_size // 4)
+        self.fc5 = nn.Linear(hidden_size // 4, output_size)
+        self.relu = nn.ReLU()  # ReLU activation function
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
+        x = self.relu(x)
+        x = self.fc5(x)
+        x = torch.tanh(x)  # Enforces the action range between -1 and 1
+        return x
