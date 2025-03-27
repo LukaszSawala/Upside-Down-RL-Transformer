0fead3da158993f6bbebe0a24b4e2b76849ec74b
LukaszSawala <lukaszsawala2003@gmail.com>
2025-03-27 12:45:31 +0100

    adding DT code, adjusting the dataset to contain rewards

--- lukasz_sawala_bsc_thesis/models.py
+++ lukasz_sawala_bsc_thesis/models.py
@@ -5,7 +5,7 @@ import torch
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)  
+        self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
         self.fc3 = nn.Linear(hidden_size, hidden_size // 2)
         self.fc4 = nn.Linear(hidden_size // 2, hidden_size // 4)
